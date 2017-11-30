df4c7cb366b9ed9d027a889de64189426ae38d58
Nick Petosa <nick.petosa@gmail.com>
2017-11-30 01:17:31 -0500

    Writing report

--- explore.py
+++ explore.py
@@ -15,8 +15,11 @@ print()
 # Plot data
 df_sample = analysis.sample(df, 1000, ["route", "stop", "session", "approach"])
 analysis.plot(df_sample, gt_context, "route", ("actualSecondsToArrival", "abserror"), 1)
-'''
-analysis.plot(df_sample, gt_context, "route", ("isDeparture", "actualSecondsToArrival"), 3)
+analysis.plot(df_sample, gt_context, "route", ("distance", "abserror"), 1)
+analysis.plot(df_sample, gt_context, "route", ("kmperhr", "abserror"), 1)
+analysis.plot(df_sample, gt_context, "route", ("minutesIntoDay", "abserror"), 1)
+
+analysis.plot(df_sample, gt_context, "route", ("layover", "actualSecondsToArrival"), 3)
 analysis.plot(df_sample, gt_context, "route", ("wind", "actualSecondsToArrival"), 3)
 analysis.plot(df_sample, gt_context, "route", ("pressure", "actualSecondsToArrival"), 3)
 analysis.plot(df_sample, gt_context, "route", ("humidity", "actualSecondsToArrival"), 3)
@@ -24,7 +27,7 @@ analysis.plot(df_sample, gt_context, "route", ("visibility", "actualSecondsToArr
 analysis.plot(df_sample, gt_context, "route", ("secondsToArrival", "actualSecondsToArrival"), 2)
 analysis.plot(df_sample, gt_context, "cluster", ("secondsToArrival", "actualSecondsToArrival"), 2)
 
-'''
+
 # Ancillary plots, showing heuristic validation and distance validation
 df_sample = analysis.sample(df, 10, ["route", "stop", "session"])
 analysis.plot_validation(df_sample)
